8c86d1be35f6d985245bbcfcbf93acc7287e1b24
YogehswariAllapure <yallapure@gmail.com>
2021-03-17 17:09:45 +0530

    update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        icon: path.join(app.getAppPath(), 'assets/logo.png'),
+        icon: path.join(app.getAppPath(), 'src/assets/logo.png'),
         webPreferences: {
             nodeIntegration: true
         }
